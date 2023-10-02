0c35579f202012bc985b6bd54e3e3f8e98d58aa5
jaypat3 <71624115+jaypat3@users.noreply.github.com>
2023-10-02 18:55:40 -0400

    Update no way this doesn't work.py

--- no way this doesn't work.py
+++ no way this doesn't work.py
@@ -4,257 +4,206 @@ from dstructure import *
 from ddstructure import *
 from braid import *
 
-Z = PMC([(0,2),(1,3),(4,6),(5,7)])
-W = PMC([(0, 3), (1, 6), (2, 4), (5, 7)])
-ddstr_list = [] #my first attempt to caluclate using computeDD. This list should hold four DD structures: T2, T2, T1^-1, T2^-1
-#ddstr_list2 = [] #my second attempt to calculate using computeDD. This list should hold 54 DD structures given by the 54 arcslides
-
-#DD structure for T1
-
-pairs1 = []
-pairs1.append([7,6])
-pairs1.append([7,6])
-pairs1.append([7,6])
-pairs1.append([7,6])
-pairs1.append([7,6])
-pairs1.append([7,6])
-
-def computeT1helper(Y, ddstr2, index, isInverse):
-    #Y: PMC
-    #ddstr1: DD structure on LHS
-    #ddstr2: DD structure on RHS
-    #index: tells us which pair of numbers in the array to use to generate the DD structure on LHS
-    #isInverse: true if we're computing T1^-1; false if we're computing T1
-    if isInverse == True:
-        slide = Arcslide(Y, pairs1[index][0], pairs1[index][1]).inverse()
-    else:
-        slide = Arcslide(Y, pairs1[index][0], pairs1[index][1])
-       
-    ddstr1 = slide.getDDStructure()
-    #ddgraph1 = TypeDDGraph(ddstr1, 2)
-   
-    #aa = TypeAAGraph(Y)
-    product = computeDATensorDD(ddstr1, ddstr2)
-    product.simplify()
-    product.reindex()
-    #result = TypeDDGraph(product, 1)
-    return product.algebra1.pmc, product
-
-def computeT1(Y, ddstr2, isInverse):
-    #Y: PMC
-    #ddgraph: rightmost DD structure
-    #isInverse: true if we're computing T1^-1; false if we're computing T1
-    pmc, prod = Y, ddstr2
-    for i in range (1, 6):
-        pmc, prod = computeT1helper(pmc, prod, i, isInverse)
-        #ddstr_list2.append(prod)
-        print('counter: ', i)
-        print(pmc)
-    return pmc, prod
-
-slide1 = Arcslide(Z, 7, 6)
-ddstr1 = slide1.getDDStructure()
-ddstr1.simplify()
-#ddgraph1 = TypeDDGraph(ddstr1, 1)
-
-final_pmc1, final_product1= computeT1(Z, ddstr1, False)
-print("final pmc: ", final_pmc1)
-
-#DD structure for T1 inverse
-
-slide1 = Arcslide(Z, 7, 6).inverse()
-ddstr1 = slide1.getDDStructure()
-#ddstr1.simplify()
-
-#ddgraph1 = TypeDDGraph(ddstr1, 1)
-#print(ddstr1.algebra2.opp())
-
-#ddstr_list2.append(ddstr1)
-
-final_pmc1_inv, final_product1_inv = computeT1(Z, ddstr1, True)
-
-#print(len(ddstr_list2))
-
-#DD structure for T2
-pairs2 = []
-pairs2.append([4,3])
-pairs2.append([4,5])
-pairs2.append([3,4])
-pairs2.append([2,1])
-pairs2.append([3,2])
-pairs2.append([4,3])
-pairs2.append([6,5])
-pairs2.append([3,2])
-pairs2.append([4,3])
-pairs2.append([5,4])
-pairs2.append([7,6])
-pairs2.append([4,3])
-pairs2.append([5,4])
-pairs2.append([6,5])
-print(pairs2)
-
-#DD structure for T2
-
-def computeT2helper(Y, ddstr1, index):
-    #Y: PMC
-    #ddstr1: DD structure on LHS
-    #ddstr2: DD structure on RHS
-    #index: tells us which pair of numbers in the array to use to generate the DD structure on LHS
-   
-    slide = Arcslide(Y, pairs2[index][0], pairs2[index][1])
-    ddstr2 = slide.getDDStructure()
-       
-    #ddgraph2 = TypeDDGraph(ddstr2, 1)
-    #print('left dd structure PMC: ', ddgraph2.algebra1)
-    #print('right dd structure PMC: ', ddgraph1.algebra2.opp())
-   
-    #aa = TypeAAGraph(Y)
-    product = computeDATensorDD(ddstr1, ddstr2)
-    product.simplify()
-    product.reindex()
-    #result = TypeDDGraph(product, 2)
-    return product.algebra2.opp().pmc, product
-
-def computeT2(Y, ddstr1):
-    #Y: PMC
-    #ddstr1: leftmost DD structure
-    pmc, prod= Y, ddstr1
-    for i in range (1, 14):
-        pmc, prod = computeT2helper(pmc, prod, i)
-        #ddstr_list2.append(prod)
-        PMClist.append(pmc)
-        print('counter: ', i)
-        print(pmc)
-    return pmc, prod
-
-PMClist = []
-PMClist.append(Z)
-PMClist.append(W)
-slide1 = Arcslide(Z, 4, 3)
-ddstr1 = slide1.getDDStructure()
-#ddstr1.simplify()
-
-
-pmc_final2, product2_final = computeT2(W, ddstr1)
-print("final pmc: ", pmc_final2)
-
-#ddstr_list2.reverse()
-#print(len(ddstr_list2))
-
-print(len(PMClist)) #There are 14 arcslides, 15 PMCs. Makes sense.
-print(PMClist)
-
-#DD structure for T2^-1
-#helper_list = []
-
-def computeT2helper_inv(ddstr1, index):
-    #ddstr1: DD structure on LHS
-    #ddstr2: DD structure on RHS
-    #index: tells us which pair of numbers in the array to use to generate the DD structure on RHS
-   
-    #We use the stored PMCs in PMCList to generate the Arcslides, before inverting them
-    slide = Arcslide(PMClist[13-index], pairs2[13-index][0], pairs2[13-index][1]).inverse()
-    ddstr2 = slide.getDDStructure()
-    #ddstr2.simplify()
-    #ddstr_list.append(ddstr2)
-    #ddgraph2 = TypeDDGraph(ddstr2, 1)
-    #print('left dd structure PMC: ', ddgraph2.algebra1)
-    #print('right dd structure PMC: ', ddgraph1.algebra2.opp())
-   
-   
-    #aa = TypeAAGraph(PMClist[14-index])
-    product = computeDATensorDD(ddstr1, ddstr2)
-    product.simplify()
-    product.reindex()
-    #result = TypeDDGraph(product, 2)
-    return product.algebra2.opp().pmc, product
-
-def computeT2_inv(ddstr1):
-    #Y: PMC
-    #ddstr1: leftmost DD structure
-    prod = ddstr1
-    for i in range (1, 14):
-        pmc, prod = computeT2helper_inv(prod, i)
-        #helper_list.append(prod)
-        print('counter: ', i)
-        print(pmc)
-    return pmc, prod
-
-
-slide1 = Arcslide(PMClist[13], 6, 5).inverse()
-ddstr1 = slide1.getDDStructure()
-#ddstr1.simplify()
-#helper_list.append(ddstr1)
-#ddgraph1 = TypeDDGraph(ddstr1, 2)
-
-pmc_final2_inv, product2_final_inv = computeT2_inv(ddstr1)
-print("final pmc: ", pmc_final2_inv)
+#manually calculated Arcslides
+Z = PMC([(0,2),(1,3), (4,6) ,(5,7)])
+W = PMC([(0, 3), (1, 6), (2, 4),  (5, 7)])
 
 #T1
-final_pmc1, final_product1
-#final_product1.simplify()
+Slide1 = Arcslide(Z, 4, 5)
+Slide2 = Arcslide(Z, 4, 5)
+Slide3 = Arcslide(Z, 4, 5)
+Slide4 = Arcslide(Z, 4, 5)
+Slide5 = Arcslide(Z, 4, 5)
+Slide6 = Arcslide(Z, 4, 5)
+
+slides_T1 = []
+slides_T1.append(Slide1)
+slides_T1.append(Slide2)
+slides_T1.append(Slide3)
+slides_T1.append(Slide4)
+slides_T1.append(Slide5)
+slides_T1.append(Slide6)
+
+ddstr_list_T1 = []
+for slide in slides_T1:
+    ddstr_list_T1.append(slide.getDDStructure())
+print(len(ddstr_list_T1))
 
 #T1^-1
-final_pmc1_inv, final_product1_inv
-#final_product1_inv.simplify()
+Slide7 = Arcslide(Z, 7, 6)
+Slide8 = Arcslide(Z, 7, 6)
+Slide9 = Arcslide(Z, 7, 6)
+Slide10 = Arcslide(Z, 7, 6)
+Slide11 = Arcslide(Z, 7, 6)
+Slide12 = Arcslide(Z, 7, 6)
+slides_T1inv = []
+slides_T1inv.append(Slide7)
+slides_T1inv.append(Slide8)
+slides_T1inv.append(Slide9)
+slides_T1inv.append(Slide10)
+slides_T1inv.append(Slide11)
+slides_T1inv.append(Slide12)
+
+ddstr_list_T1inv = []
+for slide in slides_T1inv:
+    ddstr_list_T1inv.append(slide.getDDStructure())
+print(len(ddstr_list_T1inv))
+
+#T1^-1 ONE HALF
+Slide1 = Arcslide(Z, 7, 6)
+Slide2 = Arcslide(Z, 7, 6)
+Slide3 = Arcslide(Z, 7, 6)
+slides_T1invhalf = []
+slides_T1invhalf.append(Slide1)
+slides_T1invhalf.append(Slide2)
+slides_T1invhalf.append(Slide3)
+
+ddstr_list_T1invhalf = []
+for slide in slides_T1invhalf:
+    ddstr_list_T1invhalf.append(slide.getDDStructure())
+print(len(ddstr_list_T1invhalf))
+
 
 #T2
-pmc_final2, product2_final
-#product2_final.simplify()
+slide1 = Arcslide(W, 1, 2)
+slide2 = Arcslide(PMC([(0, 3), (1, 5), (2, 7), (4, 6)]), 7, 6)
+slide3 = Arcslide(PMC([(0, 6), (1, 4), (2, 7), (3, 5)]), 6, 5)
+slide4 = Arcslide(PMC([(0, 6), (1, 4), (2, 5), (3, 7)]), 3, 4)
+slide5 = Arcslide(PMC([(0, 6), (1, 3), (2, 5), (4, 7)]), 4, 5)
+slide6 = Arcslide(PMC([(0, 6), (1, 7), (2, 4), (3, 5)]), 1, 2)
+slide7 = Arcslide(PMC([(0, 3), (1, 7), (2, 5), (4, 6)]), 3, 4)
+slide8 = Arcslide(PMC([(0, 4), (1, 7), (2, 5), (3, 6)]), 4, 5)
+slide9 = Arcslide(PMC([(0, 5), (1, 7), (2, 4), (3, 6)]), 5, 6)
+slide10 = Arcslide(PMC([(0, 2), (1, 7), (3, 5), (4, 6)]), 2, 3)
+slide11 = Arcslide(PMC([(0, 2), (1, 4), (3, 6), (5, 7)]), 4, 5)
+slide12 = Arcslide(PMC([(0, 2), (1, 5), (3, 6), (4, 7)]), 5, 6)
+slide13 = Arcslide(PMC([(0, 2), (1, 6), (3, 5), (4, 7)]), 6, 7)
+slide14 = Arcslide(PMC([(0, 2), (1, 3), (4, 6), (5, 7)]), 3, 4)
+slides_T2 = []
+slides_T2.append(slide1)
+slides_T2.append(slide2)
+slides_T2.append(slide3)
+slides_T2.append(slide4)
+slides_T2.append(slide5)
+slides_T2.append(slide6)
+slides_T2.append(slide7)
+slides_T2.append(slide8)
+slides_T2.append(slide9)
+slides_T2.append(slide10)
+slides_T2.append(slide11)
+slides_T2.append(slide12)
+slides_T2.append(slide13)
+slides_T2.append(slide14)
+
+ddstr_list_T2 = []
+for slide in slides_T2:
+    ddstr_list_T2.append(slide.getDDStructure())
+print(len(ddstr_list_T2))
 
 #T2^-1
-pmc_final2_inv, product2_final_inv
-#product2_final_inv.simplify()
+slide15 = Arcslide(PMC([(0, 2), (1, 6), (3, 5), (4, 7)]), 6, 5)
+slide16 = Arcslide(PMC([(0, 2), (1, 5), (3, 6), (4, 7)]), 5, 4)
+slide17 = Arcslide(PMC([(0, 2), (1, 4), (3, 6), (5, 7)]), 4, 3)
+slide18 = Arcslide(PMC([(0, 2), (1, 7), (3, 5), (4, 6)]), 7, 6)
+slide19 = Arcslide(PMC([(0, 5), (1, 7), (2, 4), (3, 6)]), 5, 4)
+slide20 = Arcslide(PMC([(0, 4), (1, 7), (2, 5), (3, 6)]), 4, 3)
+slide21 = Arcslide(PMC([(0, 3), (1, 7), (2, 5), (4, 6)]), 3, 2)
+slide22 = Arcslide(PMC([(0, 6), (1, 7), (2, 4), (3, 5)]), 6, 5)
+slide23 = Arcslide(PMC([(0, 6), (1, 3), (2, 5), (4, 7)]), 4, 3)
+slide24 = Arcslide(PMC([(0, 6), (1, 4), (2, 5), (3, 7)]), 3, 2)
+slide25 = Arcslide(PMC([(0, 6), (1, 4), (2, 7), (3, 5)]), 2, 1)
+slide26 = Arcslide(PMC([(0, 3), (1, 5), (2, 7), (4, 6)]), 3, 4)
+slide27 = Arcslide(PMC([(0, 3), (1, 6), (2, 4), (5, 7)]), 4, 5)
+slide28 = Arcslide(Z, 4, 3)
 
-# ddstr_list.append(product2_final)
-# ddstr_list.append(product2_final_inv)
-ddstr_list.append(product2_final_inv)
-ddstr_list.append(product2_final_inv)
-ddstr_list.append(product2_final_inv)
-ddstr_list.append(product2_final_inv)
-ddstr_list.append(final_product1)
-ddstr_list.append(product2_final)
-# ddstr_list.append(product2_final)
-# ddstr_list.append(product2_final)
-# ddstr_list.reverse()
+slides_T2inv = []
+slides_T2inv.append(slide15)
+slides_T2inv.append(slide16)
+slides_T2inv.append(slide17)
+slides_T2inv.append(slide18)
+slides_T2inv.append(slide19)
+slides_T2inv.append(slide20)
+slides_T2inv.append(slide21)
+slides_T2inv.append(slide22)
+slides_T2inv.append(slide23)
+slides_T2inv.append(slide24)
+slides_T2inv.append(slide25)
+slides_T2inv.append(slide26)
+slides_T2inv.append(slide27)
+slides_T2inv.append(slide28)
 
-# newlist = []
-# for i in range(0,len(ddstr_list)):
-    # newlist.append(ddstr_list[len(ddstr_list)-1-i])
+ddstr_list_T2inv = []
+for slide in slides_T2inv:
+    ddstr_list_T2inv.append(slide.getDDStructure())
+print(len(ddstr_list_T2inv))
 
-"""
-#T2*T2
-aa = TypeAAGraph(Z)
-graphT2_1 = TypeDDGraph(final_product1, 2)
-graphT2_2 = TypeDDGraph(final_product1, 1)
-productT2 = aa.tensorDoubleDD(graphT2_1, graphT2_2)
-productT2.simplify()
-productT2_graph = TypeDDGraph(productT2, 1)
-print(productT2_graph.algebra1)
-print(productT2_graph.algebra2.opp())
-"""
+# ddstr_list = ddstr_list_T2 + ddstr_list_T2 + ddstr_list_T1inv + ddstr_list_T2inv
+# ddstr_list = ddstr_list_T2 + ddstr_list_T2 + ddstr_list_T2 + ddstr_list_T2 + ddstr_list_T1inv + ddstr_list_T2inv + ddstr_list_T2inv + ddstr_list_T2inv
+ddstr_list = ddstr_list_T2 + ddstr_list_T2 + ddstr_list_T1inv + ddstr_list_T2inv
+#this is CFDhat(H^2, phi_2)
 dstr = zeroTypeD(2)
-dgraph = TypeDGraph(dstr)
+"""
+#running some tests
+ddstr_list_test1 = ddstr_list_T2 + ddstr_list_T2inv
+ddstr_list_test2 = ddstr_list_T2inv + ddstr_list_T2
+ddstr_list_test3 = ddstr_list_T1 + ddstr_list_T1inv
+ddstr_list_test4 = ddstr_list_T1inv + ddstr_list_T1
 
-print("LEN DDSTR_LIST: ", len(ddstr_list))
+dstr_test1 = composeDD(dstr, ddstr_list_test1, is_dual = False, method = "Tensor")
+dstr_test2 = composeDD(dstr, ddstr_list_test2, is_dual = False, method = "Tensor")
+dstr_test3 = composeDD(dstr, ddstr_list_test3, is_dual = False, method = "Tensor")
+dstr_test4 = composeDD(dstr, ddstr_list_test4, is_dual = False, method = "Tensor")
 
+print('test1:', dstr_test1.compareDStructures(dstr))
+print('test2:', dstr_test2.compareDStructures(dstr))
+print('test3:', dstr_test3.compareDStructures(dstr))
+print('test4:', dstr_test4.compareDStructures(dstr))
+"""
+#this gives us CFD hat of the drilled manifold
 final_dstr = composeDD(dstr, ddstr_list, is_dual = False, method = "Tensor")
-
-
-# final_dstr.simplify()
-# final_dstr.reindex()
-
-
+final_dstr.simplify()
+final_dstr.reindex()
 print(final_dstr)
 
+ddstr = DDStrFromDStr(final_dstr, 1)
 """
-print(final_dstr.delta_map)
-print(final_dstr.generators)
+#I copied down the code for the DStrtoDDStr method
+genus1 = 1
+pmc_all = final_dstr.algebra.pmc
+pmc1, pmc2 = unconnectSumPMC(pmc_all, genus1)
+mult_one = final_dstr.algebra.mult_one
+
+ddstr = SimpleDDStructure(F2, pmc1.getAlgebra(mult_one = mult_one), pmc2.getAlgebra(mult_one = mult_one))
+gen_map = {}
+for x in final_dstr.getGenerators():
+    # Split idempotent of x into two parts
+    xidem = x.idem
+    x1_idem = Idempotent(pmc1,[pairid for pairid in xidem if pairid < 2*genus1])
+    x2_idem = Idempotent(pmc2,[pairid-2*genus1 for pairid in xidem if pairid >= 2*genus1])
+    if len(x1_idem) != genus1:
+        continue
+    gen_map[x] = SimpleDDGenerator(ddstr, x1_idem, x2_idem, x.name)
+    ddstr.addGenerator(gen_map[x])
+
+cut_point = 4 * genus1
+for x in final_dstr.getGenerators():
+    for (a, y), coeff in list(x.delta().items()):
+        print('cut point', a.multiplicity[cut_point-1], y)
+        if a.multiplicity[cut_point-1] == 0:
+            # The interval (cut_point-1, cut_point) is unoccupied
+            a1, a2 = unconnectSumStrandDiagram(a, genus1)
+            print(x)
+            print(a, y)
+            print(a1, a2)
+            print(str(a1), str(a2))
+            if str(a1) !='[]' and str(a2) != '[]': #Here I added a check to make sure we don't get keyErrors
+                ddstr.addDelta(gen_map[x], gen_map[y], a1, a2, coeff)
+            else:
+                print('not happen')
+print(ddstr)
 """
 
-dd = DDStrFromDStr(final_dstr, 1)
-dd.simplify()
-dd.reindex()
-
-print(dd)
-
+ddstr.simplify()
+ddstr.reindex()
+dd_printed = str(ddstr)[19: ]
+print(dd_printed)
